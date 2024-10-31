ecd014f1f64db12b0058012eda55d1c56e3d5f53
jsimpauco <jaredsimpauco@Jareds-Air.lan>
2024-10-31 16:54:38 -0700

    added data json file and added script to run the clean_data function

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 
 # Imports #
 import sys
+import json
 import src.data as data
 
 # Checking if script.py is being run as a script in command line #
@@ -9,8 +10,13 @@ if __name__ == '__main__':
 
     # Getting args #
     args = sys.argv[1:]
-    # print(args)
 
     # data argument #
     if 'data' in args:
+
+        # downloading data #
         data.download_data()
+
+        # cleaning data #
+        config = json.load(open('config/data.json'))
+        data.clean_data(**config)
