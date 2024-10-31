d062621085338531a049fd7978277d0bdea3abe6
jsimpauco <jaredsimpauco@Jareds-MacBook-Air.local>
2024-10-31 14:17:25 -0700

    created script file

---
+++ script.py
@@ -0,0 +1,9 @@
+# Imports #
+import sys
+
+# Checking if script.py is being run as a script in command line #
+if __name__ == '__main__':
+
+    # Getting args #
+    args = sys.argv[1:]
+    # print(args)
