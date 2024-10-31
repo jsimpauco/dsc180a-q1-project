7bc193314b00148bc0834ed533247581fe626d7a
jsimpauco <jaredsimpauco@Jareds-Air.lan>
2024-10-31 16:32:40 -0700

    updated data file to no longer download data if file already exists

--- src/data.py
+++ src/data.py
@@ -7,11 +7,16 @@ data.py retrieves and cleans the data required for this project
 # Imports #
 from datasets import load_dataset
 import pandas as pd
+import os.path
 
 def download_data():
     """
     Downloads the required data
     """
+
+    # Checks if data is already downloaded #
+    if os.path.isfile('data/raw_data.csv'):
+        return
 
     # Loading dataset #
     ds = load_dataset("songlab/genomes-brassicales-balanced-v1")
@@ -20,4 +25,4 @@ def download_data():
     df = ds['train'].to_pandas()
     
     # Exporting to data folder #
-    df.to_csv('data/raw_data', index=False)
+    df.to_csv('data/raw_data.csv', index=False)
